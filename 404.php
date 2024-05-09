74b0b317d00c13d468f157348bfb29d777b827e6
Anurag <anuragtyagi014@gmail.com>
2024-05-09 13:11:09 +0530

    404 page issue fixed

--- 404.php
+++ 404.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying 404 pages (not found)
  *
@@ -9,16 +10,17 @@
 
 namespace Kadence;
 
+
 get_header();
 
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
 	exit;
 }
 
-kadence()->print_styles( 'kadence-content' );
+kadence()->print_styles('kadence-content');
 /**
  * Hook for everything, makes for better elementor theming support.
  */
-do_action( 'kadence_single' );
+do_action('kadence_single');
 
 get_footer();
